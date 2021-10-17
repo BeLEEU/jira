8dbb41cef829ba03bbe44deabdd11cd2fdf5e540
BeLEEU <531357870@qq.com>
2021-10-17 09:23:39 +0800

    add useDebounce

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { List } from "./list"
+import { SearchPanel } from "./search-panel"
+import {useState, useEffect} from 'react'
+import * as qs from 'qs'
+import { cleanObject, useDebounce } from 'utils'
+
+const apiUrl = process.env.REACT_APP_API_URL
+
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: '',
+    personId: ''
+  })
+  const [users, setUsers] = useState([])
+  const [list, setList] = useState([])
+  const debouncedParam = useDebounce(param, 500)
+
+  useEffect(() =>{
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
+      if(response.ok){
+        setList(await response.json())
+      }
+    })
+  }, [debouncedParam])
+
+  useEffect(() =>{
+    fetch(`${apiUrl}/users`).then(async response => {
+      if(response.ok){
+        setUsers(await response.json())
+      }
+    })
+  }, [])
+
+  return <div>
+    <SearchPanel param={param} setParam={setParam} users={users}/>
+    <List list={list} users={users}/>
+  </div>
+}
