932f7fae7b2c1d47a8f3d3036798594500eb48b9
BeLEEU <531357870@qq.com>
2021-11-02 22:48:00 +0800

    auth页面布局

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,13 +1,46 @@
+import styled from "@emotion/styled";
 import { Button } from "antd";
 import { useAuth } from "context/auth-context"
 import { ProjectListScreen } from "screens/project-list"
+import {Row} from 'components/lib'
 
 export const AuthenticatedApp = () => {
 
   const {logout} = useAuth();
 
   return <div>
-    <Button onClick={logout}>登出</Button>
-    <ProjectListScreen/>
+    <Header between={true}>
+      <HeaderLeft gap={true}>
+        <h2>Logo</h2>
+        <h2>项目</h2>
+        <h2>用户</h2>
+      </HeaderLeft>
+      <HeaderRight>
+        <Button onClick={logout}>登出</Button>
+      </HeaderRight>
+    </Header>
+    <Main>
+      <ProjectListScreen/>
+    </Main>    
   </div>
 }
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  height: 100vh;
+`
+const Header = styled(Row)`
+`
+
+const HeaderLeft = styled(Row)`
+  display: flex;
+  align-items: center;
+`
+const HeaderRight = styled.div`
+
+`
+const Main = styled.main`
+  grid-area: main;
+`
+
